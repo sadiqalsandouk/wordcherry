b9539a149a6c4e37615a48f9f87f2286844c9baa
Sadiq <sadiqalsandouk@hotmail.com>
2025-07-03 23:07:17 +0100

    replaced useRouter with Link

--- app/components/SoloButton.tsx
+++ app/components/SoloButton.tsx
@@ -1,16 +1,14 @@
 "use client"
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 export default function SoloButton() {
-  const router = useRouter()
   return (
     <div className="mx-auto max-w-sm px-4 mt-4">
-      <button
-        onClick={() => router.push("/solo")}
-        className="cursor-pointer w-full bg-applegramYellow text-applegramBlue font-bold text-3xl py-4 rounded-xl shadow-[2px_2px_0_rgba(0,0,0,0.15),0_1px_2px_rgba(0,0,0,0.1)] hover:bg-applegramYellow/90"
-      >
-        PLAY SOLO
-      </button>
+      <Link href="/solo" className="block">
+        <button className="cursor-pointer w-full bg-applegramYellow text-applegramBlue font-bold text-3xl py-4 rounded-xl shadow-[2px_2px_0_rgba(0,0,0,0.15),0_1px_2px_rgba(0,0,0,0.1)] hover:bg-applegramYellow/90">
+          PLAY SOLO
+        </button>
+      </Link>
     </div>
   )
 }
