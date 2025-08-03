570eb50e0f1fa7ac9a8361b7552fd2d17c91f981
Sadiq Al-Sandouk <sadiqalsandouk@hotmail.com>
2025-08-04 00:17:54 +0100

    pivot game removing board

--- app/solo/page.tsx
+++ app/solo/page.tsx
@@ -1,11 +1,7 @@
-import GameBoard from "../components/GameBoard"
-
 export default function SoloPage() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-applegramBlue p-8">
-      <div className="w-[800px] bg-white rounded-xl shadow-lg p-6">
-        <GameBoard />
-      </div>
+        SOLO PAGE
     </div>
   )
 }
